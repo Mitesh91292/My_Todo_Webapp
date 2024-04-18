5bf2841774e4b409ff33bb53888e1fd496b22804
mitesh <miteshg43@gmail.com>
2024-04-18 16:27:10 +0530

    remove subheader

--- web.py
+++ web.py
@@ -14,7 +14,7 @@ def add_todo():
     st.session_state["new_todo"] = ""
 
 st.title("My Todo App")
-st.subheader("This is my todo app")
+#st.subheader("This is my todo app")
 st.write("This app is to increase your productivity")
 
 for index, todo in enumerate(todos):
